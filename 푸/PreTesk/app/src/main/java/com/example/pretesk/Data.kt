c403136c834a9d26f189507dcedcd872b7ab7fa9
아딘컴\김아린 <jyandysys1@naver.com>
2023-01-12 19:10:44 +0900

    KaKao 로그인

--- 푸/PreTesk/app/src/main/java/com/example/pretesk/Data.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pretesk
-
-data class Data(
-    var title: String,
-    var desc: String,
-)
--- 푸/Pre_tesk2/app/src/main/java/com/example/pre_tesk2/ApiInterface.kt
+++
@@ -1,9 +0,0 @@
-package com.example.pre_tesk2
-
-import retrofit2.Response
-import retrofit2.http.GET
-
-interface ApiInterface {
-    @GET("/users")
-    suspend fun getUserData() : Response<List<User>>
-}
